3b068a47bf08c30db0c3c2a8440854f934468b2f
T-MONIKA <tmonikat2810@gmail.com>
2026-02-11 14:37:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,38 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "monigame" {
-  ami           = "ami-073130f74f5ffb161" 
-  instance_type = "t3.small"
-  key_name      = "M"
+# Get Latest Ubuntu AMI Automatically
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-  security_groups = [aws_security_group.monigame.name]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install nginx -y
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
-
-  tags = {
-    Name = "monigame"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
+# Security Group
 resource "aws_security_group" "monigame" {
-  name = "monigame"
+  name        = "monigame"
+  description = "Allow HTTP and SSH"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -46,6 +45,29 @@ resource "aws_security_group" "monigame" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# EC2 Instance
+resource "aws_instance" "monigame" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.small"
+  key_name      = "M"
+
+  vpc_security_group_ids = [aws_security_group.monigame.id]
+
+  user_data = <<-EOT
+    #!/bin/bash
+    apt update -y
+    apt install nginx -y
+    systemctl start nginx
+    systemctl enable nginx
+  EOT
+
+  tags = {
+    Name = "monigame"
+  }
+}
+
+# Output Public IP
 output "public_ip" {
   value = aws_instance.monigame.public_ip
 }
