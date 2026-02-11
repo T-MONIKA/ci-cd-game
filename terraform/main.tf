ef9b3052d99935947b6791e2488a3aee045d318d
T-MONIKA <tmonikat2810@gmail.com>
2026-02-11 11:07:43 +0530

    Rename resources and update security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 resource "aws_instance" "game_server" {
   ami           = "ami-073130f74f5ffb161" 
   instance_type = "t3.small"
-  key_name      = "monz"
+  key_name      = "M"
 
-  security_groups = [aws_security_group.mygame.name]
+  security_groups = [aws_security_group.monigame.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "mygame" {
-  name = "mygame"
+resource "aws_security_group" "monigame" {
+  name = "monigame"
 
   ingress {
     from_port   = 22
